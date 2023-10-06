afb87bbbeb665b408128c97c791dfdc64ac8e9c4
Santiago Chamorro <santiago.chamorro@campusucc.edu.co>
2023-10-06 18:30:51 -0500

    first commit

---
+++ agendas/añadirContacto/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AñadircontactoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'añadirContacto'
---
+++ agendas/editarContacto/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EditarcontactoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'editarContacto'
